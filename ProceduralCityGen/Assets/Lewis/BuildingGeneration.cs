d4fab0537aa27d4376bd7f605ad035ed23f71ef9
Lewis Stewart <lewisthomasstewart@gmail.com>
2021-10-27 09:21:00 +0100

    Broke building generation down into components

--- ProceduralCityGen/Assets/Lewis/BuildingGeneration.cs
+++ ProceduralCityGen/Assets/Lewis/BuildingGeneration.cs
@@ -6,17 +6,39 @@ public static class BuildingGeneration
 {
     public static Building GenerateBuilding()
     {
-        return new Building(4, 4,
-            new Room[]
-            {
-                new Room(
-                    new RectInt(0,0,4,4),
-                    new Floor[]
-                    {
-                        new Floor(0, new Wall[16])
-                    },
-                    new Roof(RoofType.Pyrimid, RoofDirection.North)
-                    )
-            });
+        return new Building(4, 4, GenerateRooms());
+    }
+
+    static Room GenerateRooms()
+    {
+        return new Room[] { GenerateSingleRoom() };
+    }
+
+    static Room GenerateSingleRoom()
+    {
+        new Room(
+            new RectInt(0, 0, 4, 4),
+            GenerateFloors(),
+            GenerateRoof());
+    }
+    
+    static Floor[] GenerateFloors()
+    {
+        return new Floor[] { GenerateSingleFloor() }; 
+    }
+
+    static Floor GenerateSingleFloor()
+    {
+        return new Floor(0, GenerateWalls());
+    }
+
+    static Walls[] GenerateWalls()
+    {
+        return new Wall[16];
+    }
+
+    static Roof GenerateRoof()
+    {
+        return new Roof(RoofType.Pyrimid, RoofDirection.North)
     }
 }
